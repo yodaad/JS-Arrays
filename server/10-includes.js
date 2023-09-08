7790daba5a431f234d3fb300f09cd5b527de45b0
yodaad@gmail.com <yodaad@gmail.com>
2023-09-08 15:51:19 -0500

    Includes file added

---
+++ server/10-includes.js
@@ -0,0 +1,7 @@
+// includes returns true if an array contains a specified value, and flase if the value is not found. It is case sensitive.
+
+const pets = ["cat", "dog", "bat"];
+
+const answer = pets.includes("dog");
+
+console.log("Answer: ", answer);
